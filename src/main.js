383d33a2bd39c8f5b087ac23e0781a54903e692a
LuanaMayumi <luh.mayumi@gmail.com>
2022-11-09 15:57:34 -0300

    finalizando gerador de senhas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log('Oi');
+import { nanoid } from "nanoid";
+import "./style.css";
+import copy from "clipboard-copy"
+
+const passwordBtn = document.querySelector("button");
+// console.log(passwordBtn);
+const displayPassword = document.querySelector("h2");
+// console.log(displayPassword);
+
+passwordBtn.addEventListener("click", () => { // console.log('clicou no botão');
+  // console.log(nanoid());
+  const randomPassword = nanoid();
+  displayPassword.innerHTML = randomPassword;
+});
+
+displayPassword.addEventListener("click", async (event) => {
+  await copy(event.target.innerHTML);
+  alert('Senha copiada');
+});
